8e812e5c2fe02b91e2ae4c783da13f00b8f8112b
Fabiola <fabiolasou.za@live.com>
2022-03-29 13:47:21 -0300

    @fabiolasoufer [Task: CREATE models]

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin {
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: String
+    public foto: string
+    public tipo: string
+}
