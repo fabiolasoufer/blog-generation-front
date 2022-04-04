380c89b509c8877baf04bc7be427dc45811bf4c9
Fabiola <fabiolasou.za@live.com>
2022-04-04 17:08:18 -0300

    @fabiolasoufer [Task: Implementação do put e delete de tema]

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,21 @@ export class TemaService {
     return this.http.get<Tema[]>('https://fabiolasouferblog.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://fabiolasouferblog.herokuapp.com/tema/${id}`, this.token)
+
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://fabiolasouferblog.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://fabiolasouferblog.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://fabiolasouferblog.herokuapp.com/tema/${id}`, this.token)
   }
 
   
